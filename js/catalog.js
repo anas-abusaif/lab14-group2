6b50911207c241b8726bb36d394315813dfe0ffc
shahed qerbaa <shaheddamer@gmail.com>
2021-06-23 15:23:27 +0300

    2functionsolve

--- js/catalog.js
+++ js/catalog.js
@@ -4,7 +4,7 @@
 
 // Set up an empty cart for use on this page.
 const cart = new Cart([]);
-
+console.log(cart)
 // On screen load, we call this method to put all of the busmall options
 // (the things in the Product.allProducts array) into the drop down list.
 function populateForm() {
@@ -13,7 +13,6 @@ function populateForm() {
   const selectElement = document.getElementById('items');
 
   for (let i in Product.allProducts) {
-    // console.log(Product.allProducts[i].name);
     let option = document.createElement('option');
     option.innerText = Product.allProducts[i].name;
     selectElement.appendChild(option);
@@ -41,10 +40,19 @@ function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
+
+  // console.log(event.target.quantity.value)
+  cart.addItem(event.target.items.value,event.target.quantity.value)
+
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
+function updateCounter() {
+
+  let countItem = document.getElementById('itemCount');
+  countItem.innerText=cart.items.length
+  // console.log(cart.items.length)
+}
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
